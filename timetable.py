56c2d0aa16843882bdea8ed0f1c8e0d0f1261bba
Fobos <95380054+Yastrah@users.noreply.github.com>
2022-10-10 23:43:54 +0300

    Issue #1 Schedule refactor
    Satisfactory scheduling system.
    Some fixes.

--- timetable.py
+++ timetable.py
@@ -14,42 +14,20 @@ logger = logging.getLogger(__name__)
 config = load_config("config/bot.ini")
 
 # schedule_stop.set()
-# def timer():
-#     while not schedule_stop.is_set():
-#         schedule.run_pending()
-#         sleep(5)
-
 schedule_stop = threading.Event()
 
 def timer(bot: Bot, loop):
-    # await bot.send_message("1990672413", "some text")
-    last_minute = -1
     asyncio.set_event_loop(loop)
-    # loop.run_forever()
+
+    schedule.every().minute.at(":00").do(lambda: loop.create_task(send_notifications(bot)))
 
     while not schedule_stop.is_set():
-        if int(str(datetime.now())[14:16]) > last_minute or int(str(datetime.now())[14:16]) == 0:
-            if last_minute == 59:
-                last_minute = 0
-            else:
-                last_minute += 1
-
-            # asyncio.run(send_notifications(bot))
-            # loop = asyncio.get_event_loop()
-            loop.create_task(send_notifications(bot))
-            # print(asyncio.get_event_loop())
-            # print(asyncio.get_event_loop().is_running())
-            # loop.run_until_complete(send_notifications(bot))
-            # asyncio.run(send_notifications(bot))
-        sleep(32)
-
+        schedule.run_pending()
+        sleep(5)
 
 
 async def send_notifications(bot: Bot):
     now_date = datetime.now().strftime(config.logger.date_format)
-    # logger.info("NOTIFICATION AT {0}".format(now_date))
-    # await bot.send_message("1990672413", "some text")
-
     data = data_base.get_now(now_date)
 
     if data is None:
@@ -60,9 +38,11 @@ async def send_notifications(bot: Bot):
         logger.debug("There is no notifications for {0}.".format(now_date))
         return
 
-    logger.debug("There is {0} notifications for {1}.".format(len(data), now_date))
+    logger.debug("There are {0} notifications for {1}.".format(len(data), now_date))
 
     for message in data:
-        logger.info(message)
-        await bot.send_message(chat_id=message["chat_id"], text=message["text"])
-
+        try:
+            await bot.send_message(chat_id=message["chat_id"], text=message["text"])
+        except Exception as e:
+            logger.error("Failed to send notification to user {0}!\n\tException: {1}".format(message["chat_id"], e))
+            continue
